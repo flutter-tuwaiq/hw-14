b166880f81f82386ae236af1da4f0797467c06bb
AhmadAlmudhi <a.almudhi1419@gmail.com>
2023-05-30 22:14:44 +0300

    Solve: homework 14

---
+++ homework_14/README.md
@@ -0,0 +1,3 @@
+# homework_14
+
+A new Flutter project.
---
+++ homework_14/lib/app/components/my_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class MyCard extends StatelessWidget {
+  const MyCard(
+      {super.key,
+      required this.name,
+      required this.username,
+      required this.profileImage,
+      required this.image});
+
+  final String name, username, profileImage, image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(50),
+        child: Card(
+          elevation: 0,
+          color: const Color.fromARGB(255, 229, 238, 250),
+          child: Column(
+            children: [
+              ListTile(
+                trailing: CircleAvatar(
+                  backgroundImage: NetworkImage(profileImage),
+                ),
+                title: Text(
+                  name,
+                  textAlign: TextAlign.end,
+                ),
+                subtitle: Text(
+                  username,
+                  textAlign: TextAlign.end,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image(
+                    image: NetworkImage(image),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
